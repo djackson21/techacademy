00d701e5feb059d8b6e62d70bc3ca33693719b9d
Derron Jackson <derron@Derrons-MacBook-Pro.local>
2024-01-25 22:59:10 -0600

    created backend entity layer for the tech academey project

---
+++ src/main/java/com/techacademy/admin/TechacademyAdminApplication.java
@@ -0,0 +1,13 @@
+package com.techacademy.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechacademyAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechacademyAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techacademy/admin/TechacademyAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techacademy.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechacademyAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
